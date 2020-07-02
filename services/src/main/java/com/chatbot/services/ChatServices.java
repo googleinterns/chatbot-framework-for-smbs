a2af936bb86e2164b0d217f5132c122b4ed43ab4
jainsagar <jainsagar@google.com>
2020-07-02 19:25:19 +0530

    Added IDMapping and proto definitions

---
+++ services/src/main/java/com/chatbot/services/ChatServices.java
@@ -0,0 +1,11 @@
+package com.chatbot.services;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatServices {
+  public static void main(final String[] args) throws Exception {
+    SpringApplication.run(ChatServices.class, args);
+  }
+}
