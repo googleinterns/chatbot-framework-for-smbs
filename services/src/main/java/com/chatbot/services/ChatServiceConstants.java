9a7bc9abd4d9a5990d11db19d3c0450a1c99ef08
jainsagar <jainsagar@google.com>
2020-07-03 21:15:57 +0530

    Added dialogflow conversation class

---
+++ services/src/main/java/com/chatbot/services/ChatServiceConstants.java
@@ -0,0 +1,37 @@
+package com.chatbot.services;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
+ public class ChatServiceConstants {
+  public static final Map<String, List<String>> EVENT_TO_CONTEXT_MAPPING = Map.of(
+      "SUGGEST_CATEGORY_CHANGE", Arrays.asList("SuggestChangeCatgeoryContext"),
+      "SUGGEST_IMAGE_UPLOAD", Arrays.asList("SuggestImageUploadContext"),
+      "GET_CALL_FEEDBACK", Arrays.asList("GetCallFeedbackContext")); 
+  public static final List<String> LIST_OF_INTENTS_WITH_INTERACTIVE_RESPONSE = new ArrayList<>(
+      Arrays.asList("Default Welcome Intent", "Default Fallback Intent", "NotGettingEnoughCalls", 
+      "ProbeIrrelevantCall", "GettingCallsFromOutsideServiceArea", "SuggestImageUpload",
+      "GetCallFeedback", "ProbeReason", "ShouldRecommendOtherOptions", "SuggestCategoryChange"));    
+  public static final String SUGGEST_SERVICE_TIME_CHANGE = "SuggestServiceTimeChange";
+  public static final String CHANGE_SERVICE_TIME = "ChangeServiceTime";
+  public static final String PROVIDE_TARGETED_QUERIES_INTENT_NAME = "ProvideTargetedQueries";
+  public static final String RECOMMEND_MORE_OPTIONS_INTENT_NAME = "RecommendMoreOptions";
+  public static final String CHANGE_CATEGORY_INTENT_NAME = "ChangeCategory";
+  public static final String EXPLAIN_MEANING_INTENT_NAME = "ExplainMeaning";
+  public static final String UNRELATED_CALL_INTENT = "CallUnrelatedToBusinessCategory";
+  public static final String SUGGEST_TARGET_AREA_CHANGE = "SuggestTargetAreaChange";
+  public static final String CHANGE_TARGET_AREA = "ChangeTargetArea";
+  public static final String CALL_FROM_OUTSIDE_SERVICE_AREA_INTENT = 
+      "CallFromOutsideServiceArea";
+  public static final String TRIGGER_EVENT_MESSAGE = "TriggerEvent";
+  public static final String SUGGEST_CATEGORY_CHANGE_EVENT = "SUGGEST_CATEGORY_CHANGE";
+  public static final String SUGGEST_IMAGE_UPLOAD_EVENT = "SUGGEST_IMAGE_UPLOAD";
+  public static final String GET_CALL_FEEDBACK_EVENT = "GET_CALL_FEEDBACK";
+  public static final String EXPECTING_IMAGES_CONTEXT = "expectingimagescontext";
+  public static final String CHAT_ISSUER = "chat@system.gserviceaccount.com";
+  public static final String PUBLIC_CERT_URL_PREFIX =
+      "https://www.googleapis.com/service_accounts/v1/metadata/x509/";
+  public static final String HANGOUTS_USER_AGENT = "Google-Dynamite";
+}
---
+++ services/src/main/java/com/chatbot/services/DialogflowServices/DialogflowConversation.java
@@ -0,0 +1,123 @@
+package com.chatbot.services.DialogflowServices;
+
+import com.chatbot.services.ChatServiceConstants;
+import com.google.cloud.dialogflow.v2.Context;
+import com.google.cloud.dialogflow.v2.ContextsClient;
+import com.google.cloud.dialogflow.v2.CreateContextRequest;
+import com.google.cloud.dialogflow.v2.DetectIntentRequest;
+import com.google.cloud.dialogflow.v2.DetectIntentResponse;
+import com.google.cloud.dialogflow.v2.QueryInput;
+import com.google.cloud.dialogflow.v2.QueryParameters;
+import com.google.cloud.dialogflow.v2.QueryResult;
+import com.google.cloud.dialogflow.v2.SessionName;
+import com.google.cloud.dialogflow.v2.SessionsClient;
+import com.google.cloud.dialogflow.v2.TextInput;
+import com.google.protobuf.Struct;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.google.cloud.dialogflow.v2.EventInput;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class DialogflowConversation {
+
+  private static final Logger logger = LoggerFactory.getLogger(DialogflowConversation.class);
+  private static String projectID;
+  private final String langCode;
+  private final String sessionID;
+
+  DialogflowConversation(final String projectIDToSet, final String langCodeToSet,
+      final String sessionIDToSet) {
+    projectID = projectIDToSet;
+    langCode = langCodeToSet;
+    sessionID = sessionIDToSet;
+  }
+
+  public DialogflowConversation(final String projectIDToSet, final String sessionIDToSet) {
+    this(projectIDToSet, "en", sessionIDToSet);
+  }
+
+  // function to get the response for a user message from dialogflow
+  public QueryResult sendMessage(final String message, final Struct payload) throws Exception {
+    try (SessionsClient sessionsClient = SessionsClient.create()) {
+      final SessionName session = SessionName.of(projectID, sessionID);
+      final TextInput.Builder textInput = TextInput.newBuilder().setText(message)
+          .setLanguageCode(langCode);
+      final QueryInput queryInput = QueryInput.newBuilder().setText(textInput).build();
+      final QueryParameters queryParameters = QueryParameters.newBuilder().setPayload(payload)
+          .build();
+      final DetectIntentRequest detectIntentRequest =
+          DetectIntentRequest.newBuilder().setSession(session.toString()).setQueryInput(queryInput)
+          .setQueryParams(queryParameters).build();
+      final DetectIntentResponse response = sessionsClient.detectIntent(detectIntentRequest);
+      return response.getQueryResult();
+    }
+  }
+
+  // function to get the response for an event from dialogflow
+  public QueryResult triggerEvent(final String event, final Struct parameters, final Struct payload)
+      throws Exception {
+    try (SessionsClient sessionsClient = SessionsClient.create()) {
+      final SessionName session = SessionName.of(projectID, sessionID);
+      final EventInput.Builder eventInput = EventInput.newBuilder()
+          .setName(event)
+          .setParameters(parameters)
+          .setLanguageCode(langCode);
+      final QueryInput queryInput = QueryInput.newBuilder().setEvent(eventInput).build();
+      final QueryParameters queryParameters = QueryParameters.newBuilder()
+          .setPayload(payload)
+          .build();
+      final DetectIntentRequest detectIntentRequest = DetectIntentRequest.newBuilder()
+          .setSession(session.toString())
+          .setQueryInput(queryInput)
+          .setQueryParams(queryParameters)
+          .build();
+      // before triggering an event, we would need to set the context to the input context of the
+      // intent that we want to be matched
+      ChatServiceConstants.EVENT_TO_CONTEXT_MAPPING.get(event)
+          .forEach(contextName -> {
+            try {
+              setContextForSession(contextName);
+            } catch (final IOException e) {
+              logger.error("Error while setting contexts for session", e);
+            }
+          });
+      final DetectIntentResponse response = sessionsClient.detectIntent(detectIntentRequest);
+      return response.getQueryResult();
+    }
+  }
+
+  // activate given context for the session
+  void setContextForSession(final String ContextName) throws IOException {
+    try (ContextsClient contextsClient = ContextsClient.create()) {
+      final SessionName parent = SessionName.of(projectID, sessionID);
+      final Context context = Context
+          .newBuilder()
+          .setName("projects/" + projectID + "/agent/sessions/" + sessionID + "/contexts/"
+          + ContextName)
+          .setLifespanCount(1)
+          .build();
+      contextsClient.createContext(CreateContextRequest.newBuilder()
+          .setParent(parent.toString())
+          .setContext(context).build());
+    }
+  }
+
+  // get the current active contexts for the session
+  public List<String> getCurrentContexts() throws Exception {
+    final List<String> contextList = new ArrayList<String>();
+    try (ContextsClient contextsClient = ContextsClient.create()) {
+      final SessionName session = SessionName.of(projectID, sessionID);
+      for (final Context context : contextsClient.listContexts(session).iterateAll()) {
+        // the name returned is the complete path of the context, of which we only need the name
+        final String[] contextNameParts = context.getName().split("/");
+        contextList.add(contextNameParts[contextNameParts.length - 1]);
+      }
+    }
+    return contextList;
+  }
+}
