9e326ba430e1164d5d284ac964c3b3c63b8cb02d
jainsagar <jainsagar@google.com>
2020-07-07 12:30:24 +0530

    Addressed nits

--- services/src/test/java/com/chatbot/services/DialogflowFulfillmentControllerTests.java
+++ services/src/test/java/com/chatbot/services/DialogflowFulfillmentControllerTests.java
@@ -37,18 +37,9 @@ public class DialogflowFulfillmentControllerTests {
   private MockMvc mockMvc;
   private final String uri = "/dgf";
   private JsonNode validRequest;
-  private JsonNode changeCategoryFulfillmentRequest;
-  private JsonNode changeTargetLocationFulfillmentRequest;
-  private JsonNode callFromOutsideServiceAreaFulfillmentRequest;
-  private JsonNode changeServiceTimeFulfillmentRequest;
-  private JsonNode explainMeanginFulfillmentRequest;
-  private JsonNode provideTargettedQueriesFulfillmentRequest;
-  private JsonNode recommendMoreOptionsFulfillmentRequest;
-  private JsonNode suggestServiceTimeChangeFulfillmentRequest;
-  private JsonNode suggestTargetAreaFulfillmentRequest;
-  private JsonNode unrelatedCallFulfillmentRequest;
   private MockHttpServletRequestBuilder mockRequestBuilder;
   private final String authToken = System.getenv("dialogflowauthtoken");
+  private final ObjectMapper mapper = new ObjectMapper();
 
   @Before
   public void setUp() throws IOException {
@@ -57,40 +48,9 @@ public class DialogflowFulfillmentControllerTests {
     mockRequestBuilder = MockMvcRequestBuilders.post(uri)
         .contentType(MediaType.APPLICATION_JSON_VALUE)
         .header("authorization", authToken);
-    ObjectMapper mapper = new ObjectMapper();
     validRequest = mapper
         .readTree(DialogflowFulfillmentControllerTests.class
         .getResourceAsStream("/fulfillmentRequest.json"));
-    changeCategoryFulfillmentRequest = mapper
-        .readTree(DialogflowFulfillmentControllerTests.class
-        .getResourceAsStream("/changeCategoryFulfillmentRequest.json"));
-    changeTargetLocationFulfillmentRequest = mapper
-        .readTree(DialogflowFulfillmentControllerTests.class
-        .getResourceAsStream("/changeTargetLocationFulfillmentRequest.json"));
-    callFromOutsideServiceAreaFulfillmentRequest = mapper
-        .readTree(DialogflowFulfillmentControllerTests.class
-        .getResourceAsStream("/callFromOutsideServiceAreaFulfillmentRequest.json"));
-    changeServiceTimeFulfillmentRequest = mapper
-        .readTree(DialogflowFulfillmentControllerTests.class
-        .getResourceAsStream("/changeServiceTimeFulfillmentRequest.json"));
-    explainMeanginFulfillmentRequest = mapper
-        .readTree(DialogflowFulfillmentControllerTests.class
-        .getResourceAsStream("/explainMeanginFulfillmentRequest.json"));
-    provideTargettedQueriesFulfillmentRequest = mapper
-        .readTree(DialogflowFulfillmentControllerTests.class
-        .getResourceAsStream("/provideTargettedQueriesFulfillmentRequest.json"));
-    recommendMoreOptionsFulfillmentRequest = mapper
-        .readTree(DialogflowFulfillmentControllerTests.class
-        .getResourceAsStream("/recommendMoreOptionsFulfillmentRequest.json"));
-    suggestServiceTimeChangeFulfillmentRequest = mapper
-        .readTree(DialogflowFulfillmentControllerTests.class
-        .getResourceAsStream("/suggestServiceTimeChangeFulfillmentRequest.json"));
-    suggestTargetAreaFulfillmentRequest = mapper
-        .readTree(DialogflowFulfillmentControllerTests.class
-        .getResourceAsStream("/suggestTargetAreaFulfillmentRequest.json"));
-    unrelatedCallFulfillmentRequest = mapper
-        .readTree(DialogflowFulfillmentControllerTests.class
-        .getResourceAsStream("/unrelatedCallFulfillmentRequest.json"));
 
   }
 
@@ -112,6 +72,9 @@ public class DialogflowFulfillmentControllerTests {
 
   @Test
   public void onEvent_changeCategoryRequest() throws Exception {
+    final JsonNode changeCategoryFulfillmentRequest = mapper
+        .readTree(DialogflowFulfillmentControllerTests.class
+        .getResourceAsStream("/changeCategoryFulfillmentRequest.json"));
     final MvcResult mvcResult = mockMvc.perform(mockRequestBuilder
         .content(changeCategoryFulfillmentRequest.toString())).andReturn();
     assertEquals(200, mvcResult.getResponse().getStatus(), "Error handling fulfillment request");
@@ -121,52 +84,79 @@ public class DialogflowFulfillmentControllerTests {
 
   @Test
   public void onEvent_callFromOutsideServiceArea() throws Exception {
+    final JsonNode callFromOutsideServiceAreaFulfillmentRequest = mapper
+        .readTree(DialogflowFulfillmentControllerTests.class
+        .getResourceAsStream("/callFromOutsideServiceAreaFulfillmentRequest.json"));
     final MvcResult mvcResult = mockMvc.perform(mockRequestBuilder
         .content(callFromOutsideServiceAreaFulfillmentRequest.toString())).andReturn();
     assertEquals(200, mvcResult.getResponse().getStatus(), "Error handling fulfillment request");
   }
+
   @Test
   public void onEvent_changeServiceTime() throws Exception {
+    final JsonNode changeServiceTimeFulfillmentRequest = mapper
+        .readTree(DialogflowFulfillmentControllerTests.class
+        .getResourceAsStream("/changeServiceTimeFulfillmentRequest.json"));
     final MvcResult mvcResult = mockMvc.perform(mockRequestBuilder
         .content(changeServiceTimeFulfillmentRequest.toString())).andReturn();
     assertEquals(200, mvcResult.getResponse().getStatus(), "Error handling fulfillment request");
     verify(asyncService).sendMessageUsingUserID("123456",
         "The service timings of your business have been changed to: 08:00 to 17:00", false);
   }
+
   @Test
   public void onEvent_explainMeaning() throws Exception {
+    final JsonNode explainMeanginFulfillmentRequest = mapper
+        .readTree(DialogflowFulfillmentControllerTests.class
+        .getResourceAsStream("/explainMeanginFulfillmentRequest.json"));
     final MvcResult mvcResult = mockMvc.perform(mockRequestBuilder
         .content(explainMeanginFulfillmentRequest.toString())).andReturn();
     assertEquals(200, mvcResult.getResponse().getStatus(), "Error handling fulfillment request");
     verify(asyncService).sendMessageUsingUserID("123456",
         "This is some information about the cafe", false);
   }
+
   @Test
   public void onEvent_provideTargettedQueries() throws Exception {
+    final JsonNode provideTargettedQueriesFulfillmentRequest = mapper
+        .readTree(DialogflowFulfillmentControllerTests.class
+        .getResourceAsStream("/provideTargettedQueriesFulfillmentRequest.json"));
     final MvcResult mvcResult = mockMvc.perform(mockRequestBuilder
         .content(provideTargettedQueriesFulfillmentRequest.toString())).andReturn();
     assertEquals(200, mvcResult.getResponse().getStatus(), "Error handling fulfillment request");
     verify(asyncService).sendMessageUsingUserID("123456",
         "These are some targeted queries for cafe", false);
   }
+
   @Test
   public void onEvent_recommendMoreOptions() throws Exception {
+    final JsonNode recommendMoreOptionsFulfillmentRequest = mapper
+        .readTree(DialogflowFulfillmentControllerTests.class
+        .getResourceAsStream("/recommendMoreOptionsFulfillmentRequest.json"));
     final MvcResult mvcResult = mockMvc.perform(mockRequestBuilder
         .content(recommendMoreOptionsFulfillmentRequest.toString())).andReturn();
     assertEquals(200, mvcResult.getResponse().getStatus(), "Error handling fulfillment request");
     verify(asyncService).sendMessageUsingUserID("123456",
         "You can also change your category to any of these:\nDiner\nHotel\nBistro", true);
   }
+
   @Test
   public void onEvent_suggestServiceTimeChange() throws Exception {
+    final JsonNode suggestServiceTimeChangeFulfillmentRequest = mapper
+        .readTree(DialogflowFulfillmentControllerTests.class
+        .getResourceAsStream("/suggestServiceTimeChangeFulfillmentRequest.json"));
     final MvcResult mvcResult = mockMvc.perform(mockRequestBuilder
         .content(suggestServiceTimeChangeFulfillmentRequest.toString())).andReturn();
     assertEquals(200, mvcResult.getResponse().getStatus(), "Error handling fulfillment request");
     verify(asyncService).sendMessageUsingUserID("123456",
         "Please choose one:\nChange to 08:00 to 20:00\nChange to 09:00 to 17:00\nChange to 10:00 to 19:00", true);
   }
+
   @Test
   public void onEvent_suggestTargetArea() throws Exception {
+    final JsonNode suggestTargetAreaFulfillmentRequest = mapper
+        .readTree(DialogflowFulfillmentControllerTests.class
+        .getResourceAsStream("/suggestTargetAreaFulfillmentRequest.json"));
     final MvcResult mvcResult = mockMvc.perform(mockRequestBuilder
         .content(suggestTargetAreaFulfillmentRequest.toString())).andReturn();
     assertEquals(200, mvcResult.getResponse().getStatus(), "Error handling fulfillment request");
@@ -174,14 +164,22 @@ public class DialogflowFulfillmentControllerTests {
         "Please choose one:\nChange target city to Hyderabad\nChange target radius to 10Km\nChange target radius to 20Km",
         true);
   }
+
   @Test
   public void onEvent_unrelatedCall() throws Exception {
+    final JsonNode unrelatedCallFulfillmentRequest = mapper
+        .readTree(DialogflowFulfillmentControllerTests.class
+        .getResourceAsStream("/unrelatedCallFulfillmentRequest.json"));
     final MvcResult mvcResult = mockMvc.perform(mockRequestBuilder
         .content(unrelatedCallFulfillmentRequest.toString())).andReturn();
     assertEquals(200, mvcResult.getResponse().getStatus(), "Error handling fulfillment request");
   }
+  
   @Test
   public void onEvent_changeTargetArea() throws Exception {
+    final JsonNode changeTargetLocationFulfillmentRequest = mapper
+        .readTree(DialogflowFulfillmentControllerTests.class
+        .getResourceAsStream("/changeTargetLocationFulfillmentRequest.json"));
     final MvcResult mvcResult = mockMvc.perform(mockRequestBuilder
         .content(changeTargetLocationFulfillmentRequest.toString())).andReturn();
     assertEquals(200, mvcResult.getResponse().getStatus(), "Error handling fulfillment request");
